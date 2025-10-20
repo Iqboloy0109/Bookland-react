fef54150012609c0d457db0bc032a88a543b854c
Iqboloy Khudoyberdieva <ikhboloy97@gmail.com>
2025-10-20 15:45:41 +0900

    feat: deploy frontend project via shell & mp2

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Production
+
+# git reset --hard
+# git checkout master
+# git pull origin master
+
+npm i yarn -g
+yarn global add serve 
+yarn
+yarn run build
+pm2 start start.js --name=BOOKLAND-REACT
